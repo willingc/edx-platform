b54b98a8ec80b881e9b4750a2551d68c0a83e6e1
noraiz-anwar <noraizbhatti@gmail.com>
2019-03-28 18:08:22 +0500

    pass grade feeze info to xblocks

---
+++ lms/djangoapps/grades/util_services.py
@@ -0,0 +1,15 @@
+"A light weight interface to grading helper functions"
+
+
+from .grade_utils import are_grades_frozen
+
+class GradesUtilService(object):
+
+
+    def __init__(self, **kwargs):
+        super(GradesUtilService, self).__init__()
+        self.course_id = kwargs.get('course_id', None)
+
+    def are_grades_frozen(self):
+        "Check if grades are frozen for given course key"
+        return are_grades_frozen(self.course_id)
